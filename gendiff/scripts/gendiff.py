a023ee1c8b0fd6feeb6dec7c88d9417303a15470
Nazar Annakhalov <nazar.annahalov@yandex.ru>
2022-07-15 19:12:07 +0300

    add content to gendiff.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -7,6 +7,7 @@ def main():
     )
     parser.add_argument('first_file')
     parser.add_argument('second_file')
+    parser.add_argument('-f', '--format', help='set format of output')
     args = parser.parse_args()
     print(args)
 
