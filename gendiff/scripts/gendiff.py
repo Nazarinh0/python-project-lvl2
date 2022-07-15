d73d33aadf21469d30848fba0e3087440eb1c9ac
Nazar Annakhalov <nazar.annahalov@yandex.ru>
2022-07-15 16:47:45 +0300

    add content to gendiff.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,2 +1,16 @@
 """Main module"""
 import argparse
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Compares two configuration files and shows a difference.'
+    )
+    parser.add_argument('integers', metavar='N', type=int, nargs='+',
+                        help='an integer for the accumulator')
+    parser.add_argument('--sum', dest='accumulate', action='store_const',
+                        const=sum, default=max,
+                        help='sum the integers (default: find the max)')
+
+
+if __name__ == '__main__':
+    main()
