845e1d20acb96863eb762264f53c8b0565ddb1a0
Nazar Annakhalov <nazar.annahalov@yandex.ru>
2022-07-18 22:38:13 +0300

    move parse and read functions to separate module

--- gendiff/parser.py
+++ gendiff/parser.py
@@ -1,5 +1,6 @@
 import json
 import yaml
+import os
 
 def parser(file_data, file_type):
     """Parse input data into .json format."""
@@ -9,3 +10,11 @@ def parser(file_data, file_type):
         '.yml': yaml.safe_load,
     }
     return mapping[file_type](file_data)
+
+
+def read_file(file):
+    """Read file and return dictionary."""
+    with open(file, 'r', encoding='utf-8') as source:
+        file_type = os.path.splitext(file)[-1]
+        file_data = source.read()
+        return parser(file_data, file_type)
