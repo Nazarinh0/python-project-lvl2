633e586981447cf7b81e7db0e9b2b9aa40f2ccca
Nazar Annakhalov <nazar.annahalov@yandex.ru>
2022-07-19 16:19:42 +0300

    add content to generator.py

--- gendiff/generator.py
+++ gendiff/generator.py
@@ -1,7 +1,17 @@
+from gendiff.diff_builder import build_diff
 from gendiff.parser import read_file
+from gendiff.formatters import formatters
 
-def generate_diff(file1, file2):
+
+def generate_diff(file1, file2, output_format):
+    if not output_format:
+        output_format = 'stylish'
     first, second = read_file(file1), read_file(file2)
-    
+    diff = build_diff(first, second)
+    formatter = formatters.get(output_format)
+    if not formatter:
+        return 'Unsupported formatter'
+    return formatter(diff)
+
 
 
