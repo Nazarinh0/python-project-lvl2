9795f827808bc24442bc6dc6c859789a47c4dc3e
Nazar Annakhalov <nazar.annahalov@yandex.ru>
2022-07-19 12:36:37 +0300

    complete stylish formatter

--- gendiff/formatters/format_stylish.py
+++ gendiff/formatters/format_stylish.py
@@ -4,4 +4,40 @@ from gendiff.tree_constants import ADDED, CHANGED, INDENT, NESTED, REMOVED, UNCH
 def stylish(diff, depth=1):
     result = []
     indent = INDENT * depth
-    for key, value in diff.items():
+    for node_key, node_value in diff.items():
+        item_type = node_value.get('type')
+        item_value = node_value.get('value')
+        if item_type == NESTED:
+            result.extend([
+                f'{indent}{UNCHANGED} {node_key}: {{',
+                stylish(item_value, depth + 2),
+                f'{indent + INDENT}}}',
+            ])
+        elif item_type == CHANGED:
+            result.extend([
+                f'{indent}{ADDED} {node_key}: {node_value.get("new_value")}',
+                f'{indent}{REMOVED} {node_key}:{node_value.get("old_value")}',
+            ])
+        else:
+            result.append('{indent}{state} {key}: {value}'.format(
+                indent=indent,
+                state=item_type,
+                key=node_key,
+                value=_get_value(item_value, depth)
+            ))
+
+
+def _get_value(item, depth=1):
+    if isinstance(item, dict):
+        return _render_array(item, depth)
+    return item
+
+
+def _render_array(items, depth):
+    result = ['{']
+    for key, value in items.items():
+        result.extend([
+            f'{INDENT * (depth + 3)}{key}:{value}',
+            f'{INDENT * (depth + 1)}}}',
+        ])
+    return '\n'.join(result)
