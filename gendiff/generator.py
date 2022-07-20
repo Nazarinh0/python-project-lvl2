40f17ecdda34f6b758791089b5a446815f82c45c
Nazar Annakhalov <nazar.annahalov@yandex.ru>
2022-07-20 18:51:50 +0300

    fix in generator.py

--- gendiff/generator.py
+++ gendiff/generator.py
@@ -3,7 +3,7 @@ from gendiff.parser import read_file
 from gendiff.formatters import formatters
 
 
-def generate_diff(file1, file2, output_format):
+def generate_diff(file1, file2, output_format=None):
     if not output_format:
         output_format = 'stylish'
     first, second = read_file(file1), read_file(file2)
